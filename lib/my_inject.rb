888133bec28e0968ffc8e3c0acd6d7fe3e02b3ac
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2014-12-21 14:17:55 +0000

    Added lastest inject update

--- lib/my_inject.rb
+++ lib/my_inject.rb
@@ -1,7 +1,15 @@
 class Array
 
-  def my_inject
-    10
+  def my_inject(*args,&block)
+    init = self[0]
+    i = 1
+    while self[i]
+      init = block.call(init,self[i])
+      i += 1
+    end
+    return init
   end
 
 end
+
+#[1.2.3.4].my_inject{|m,n| m * n }
--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -5,7 +5,24 @@ describe Array do
   it "can add numbers together" do
     expect([1,2,3,4].inject{|memory, item| memory + item}).to eq(10)
     expect([1,2,3,4].my_inject{|memory, item| memory + item}).to eq(10)
+  end
 
+  it "can add other numbers together" do
+    expect([1,2,3,4,5].inject{|memory, item| memory + item}).to eq(15)
+    expect([1,2,3,4,5].my_inject{|memory, item| memory + item}).to eq(15)
+  end
+
+ it "can minus numbers together" do
+    expect([1,2,3,4].inject{|memory, item| memory - item}).to eq(-8)
+    expect([1,2,3,4].my_inject{|memory, item| memory - item}).to eq(-8)
+  end
+
+  it "can multiply numbers together" do
+    expect([1,2,3,4].my_inject{|memory, item| memory * item}).to eq(24)
+  end
+
+  it "can add numbers together" do
+  expect([1,2,3,4].my_inject(:+)).to eq(10)
   end
 
 end
